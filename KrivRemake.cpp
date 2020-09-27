d9466789365d84471b0327ee7d4b8868275b6668
Nikita <nik.kulik2001@mail.ru>
2020-09-27 14:27:31 +0300

    Более-менее работает

--- KrivRemake.cpp
+++ KrivRemake.cpp
@@ -77,21 +77,32 @@ struct Train
 {
 	int numOfTrain;
 	char station[20];
-	Date* departureDate;
-	Time* departureTime;
-	Time* arrivalTime;
+
+	int day;
+	int month;
+
+	int departureTimeMinutes;
+	int departureTimeHours;
+
+	int arrivalTimeMinutes;
+	int arrivalTimeHours;
+
 	char type[20];
 	int freeSeats;
 
 	Train() {}
-	Train(int numOfTrain, const char Station[20], Date* departureDate, Time* departureTime, Time* arrivalTime, const char type[20], int freeSeats)
+	Train(int numOfTrain, const char Station[20], int day, int month, int departureTimeHours, int departureTimeMinutes, int arrivalTimeHours, int arrivalTimeMinutes, const char type[20], int freeSeats)
 	{
 		this->numOfTrain = numOfTrain;
 		strcpy_s(this->station, Station);
 
-		this->departureDate = departureDate;
-		this->departureTime = departureTime;
-		this->arrivalTime = arrivalTime;
+		this->day = day;
+		this->month = month;
+		this->departureTimeMinutes = departureTimeMinutes;
+		this->departureTimeHours = departureTimeHours;
+
+		this->arrivalTimeMinutes = arrivalTimeMinutes;
+		this->arrivalTimeHours = arrivalTimeHours;
 		strcpy_s(this->type, type);
 		this->freeSeats = freeSeats;
 	}
@@ -99,9 +110,56 @@ struct Train
 	{
 		printf("Номер поезда: %d\n", numOfTrain);
 		printf("Станция: "); puts(station);
-		printf("Дата отправления: %d.%d.%d\n", departureDate->day, departureDate->month, departureDate->year);
-		printf("Время отправления: %d:%d\n", departureTime->hours, departureTime->minutes);
-		printf("Время прибытия: %d:%d\n", arrivalTime->hours, arrivalTime->minutes);
+		printf("Дата отправления: ");
+		if (day < 10)
+		{
+			if (month < 10)
+				printf("0%d.0%d\n", day, month);
+			else
+				printf("0%d.%d\n", day, month);
+		}
+		else
+		{
+			if (month < 10)
+				printf("%d.0%d\n", day, month);
+			else
+				printf("%d.%d\n", day, month);
+		}
+
+
+		printf("Время отправления: ");
+		if (departureTimeHours < 10)
+		{
+			if (departureTimeMinutes < 10)
+				printf("0%d:0%d\n", departureTimeHours, departureTimeMinutes);
+			else
+				printf("0%d:%d\n", departureTimeHours, departureTimeMinutes);
+		}
+		else
+		{
+			if (departureTimeMinutes < 10)
+				printf("%d:0%d\n", departureTimeHours, departureTimeMinutes);
+			else
+				printf("%d:%d\n", departureTimeHours, departureTimeMinutes);
+		}
+		printf("Время прибытия: ");
+		if (arrivalTimeHours < 10)
+		{
+			if (arrivalTimeMinutes < 10)
+				printf("0%d:0%d\n", arrivalTimeHours, arrivalTimeMinutes);
+			else
+				printf("0%d:%d\n", arrivalTimeHours, arrivalTimeMinutes);
+		}
+		else
+		{
+			if (arrivalTimeMinutes < 10)
+				printf("%d:0%d\n", arrivalTimeHours, arrivalTimeMinutes);
+			else
+				printf("%d:%d\n", arrivalTimeHours, arrivalTimeMinutes);
+		}
+
+
+
 
 		printf("Категория: "); puts(type);
 
@@ -125,20 +183,86 @@ struct List
 	Node* tail = nullptr;
 	int count = 0;
 
-	Node* Get(int n)
+	Node* Get(int num)
 	{
-		if (n == 0)return head;
-		else if (n == count - 1)return tail;
-		else
+		Node* f = head;
+		for (int i = 0; i < num; i++)
 		{
-			Node* f = head;
-			for (int i = 0; i < n; i++)
+			f = f->next;
+		}
+		return f;
+	}
+	void Assign(int id, Train tr)
+	{
+		Node* f = head;
+		for (int i = 0; i < id; i++)
+			f = f->next;
+		f->train = tr;
+	}
+	void Sort()
+	{
+		for (int i = 0; i < count - 1; i++)
+		{
+
+			for (int j = count - 1; j > i; j--)
 			{
-				f = f->next;
+
+				if (strcmp(Get(j)->train.station, Get(j - 1)->train.station) < 0)
+				{
+					Swap(j, j - 1);
+
+
+
+				}
+
 			}
-			return f;
+
 		}
 	}
+	void SortTime()
+	{
+		for (int i = 0; i < count - 1; i++)
+		{
+
+			for (int j = count - 1; j > i; j--)
+			{
+				if (strcmp(Get(j)->train.station, Get(j - 1)->train.station) < 0)
+				{
+					if (Get(j)->train.departureTimeHours < Get(j - 1)->train.departureTimeHours)
+					{
+						Swap(j, j - 1);
+
+
+
+					}
+					else if (Get(j)->train.departureTimeHours == Get(j - 1)->train.departureTimeHours)
+					{
+						if (Get(j)->train.departureTimeMinutes < Get(j - 1)->train.departureTimeMinutes)
+							Swap(j, j - 1);
+
+					}
+				}
+			}
+
+		}
+	}
+	void Swap(int id1, int id2)
+	{
+		Node* t = head;
+		Node* n = head;
+		for (int i = 0; i < id1; i++)
+		{
+			t = t->next;
+		}
+		for (int i = 0; i < id2; i++)
+		{
+			n = n->next;
+		}
+		Train tr = n->train;
+		n->train = t->train;
+		t->train = tr;
+	}
+
 	bool Remove(int num)
 	{
 		Node* t = head;
@@ -199,27 +323,31 @@ int main()
 	const char* fileName = "information.txt";
 	fopen_s(&stream, fileName, "rb+");
 	List* list = new List();
-	/*Train* info[] = {
-		new Train(1241, "Черкизовская", new Date(21, 10, 2020), new Time(17, 59), new Time(17, 59), "Купе", 200) ,
-		new Train(2254, "Сокольники", new Date(14, 9, 2020), new Time(15, 30), new Time(0, 30), "Общий", 60),
-		new Train(5533, "Красносельская", new Date(3, 12, 2020), new Time(20, 10), new Time(20, 10), "СВ", 93),
-		new Train(4223, "Комсомольская", new Date(30, 1, 2020), new Time(22, 10), new Time(22, 10), "СВ", 43),
-		new Train(4343, "Лубянка", new Date(30, 1, 2020), new Time(22, 10), new Time(22, 10), "СВ", 43),
-		new Train(5875, "Лубянка", new Date(24, 9, 2020), new Time(18, 0), new Time(18, 50), "Плацкарт", 113),
-	};*/
-
-	printf("%d\n", _filelength(_fileno(stream)));
-	printf("%d\n", sizeof(List));
-	if (_filelength(_fileno(stream)))
+	Train* info[] = {
+		new Train(1241, "Черкизовская",      21,10,16,2,17,20                              ,"Купе", 200) ,
+		new Train(2254, "Сокольники",        21,10,18,40,19,0                             , "Общий", 60),
+		new Train(2253, "Сокольники",        21,10,21,40,22,0                             , "Общий", 60),
+		new Train(5533, "Красносельская",    21,10,22,1,22,40                              , "СВ", 93),
+		new Train(4223, "Комсомольская",     21,10,14,30,22,40                              , "СВ", 43),
+		new Train(4343, "Лубянка",           20,10,2,33,22,40                              , "СВ", 43),
+		new Train(5875, "Лубянка" ,          21,10,4,50,22,40                             ,"Плацкарт", 113),
+	};
+	int length = _filelength(_fileno(stream)) / sizeof(Train);
+	//printf("%d", length);
+	/*for (int i = 0; i < 7; i++)
 	{
-		fseek(stream, 0, 0);;
-		fread(list, sizeof(List), 1, stream);
-		list->head->train.Show();
+		fseek(stream, sizeof(Train) * i, 0);
+		fwrite(info[i], sizeof(Train), 1, stream);
+	}*/
+	for (int i = 0; i < length; i++)
+	{
+		fseek(stream, sizeof(Train) * i, 0);
+		Train* tr = new Train();
+		fread(tr, sizeof(Train), 1, stream);
+		list->Add(*tr);
+
+
 	}
-
-
-
-
 
 
 
@@ -235,15 +363,17 @@ int main()
 		printf("2 - Добавить поезд\n");
 		printf("3 - Удалить поезд\n");
 		printf("4 - Найти поезд\n");
-		printf("5 -Сохранить и выйти\n");
+		printf("5 - Сохранить и выйти\n");
 		key = _getch();
 		system("cls");
 		switch (key)
 		{
 		case '1':
 		{
-			Node* ttt = list->head;
 
+			Node* ttt = list->head;
+			list->Sort();
+			list->SortTime();
 			for (int i = 0; i < list->count; i++)
 			{
 				ttt->train.Show();
@@ -262,29 +392,26 @@ int main()
 			printf("Укажите названи города: ");
 			gets_s(train->station);
 			gets_s(train->station);
-			Date* date = new Date();
+
 			printf("Укажите дату:\n");
 			printf("День: ");
-			scanf_s("%d", &date->day);
+			scanf_s("%d", &train->day);
 			printf("Месяц: ");
-			scanf_s("%d", &date->month);
-			printf("Год: ");
-			scanf_s("%d", &date->year);
-			train->departureDate = date;
-			Time* time1 = new Time();
+			scanf_s("%d", &train->month);
+
+
 			printf("Укажите время отправления:\n");
-			printf("Минуты: ");
-			scanf_s("%d", &time1->minutes);
 			printf("Часы: ");
-			scanf_s("%d", &time1->hours);
-			train->departureTime = time1;
-			Time* time2 = new Time();
+			scanf_s("%d", &train->departureTimeHours);
+			printf("Минуты: ");
+			scanf_s("%d", &train->departureTimeMinutes);
+
+
 			printf("Укажите время прибытия:\n");
-			printf("Минуты: ");
-			scanf_s("%d", &time2->minutes);
 			printf("Часы: ");
-			scanf_s("%d", &time2->hours);
-			train->arrivalTime = time2;
+			scanf_s("%d", &train->arrivalTimeHours);
+			printf("Минуты: ");
+			scanf_s("%d", &train->arrivalTimeMinutes);
 
 			printf("Укажите тип поезда(купе,общий,плацкарт,СВ): ");
 			gets_s(train->type);
@@ -315,14 +442,25 @@ int main()
 		break;
 		case '4':
 		{
-
+			list->Get(4)->train.Show();
+			system("pause");
 		}
 		break;
 		case '5':
 		{
+			fclose(stream);
+			fopen_s(&stream, fileName, "wb+");
 			Node* t = list->head;
-			fwrite(&list, sizeof(List), 1, stream);
-			printf("Записано %d", sizeof(list));
+			for (int i = 0; i < list->count; i++)
+			{
+				fseek(stream, i * sizeof(Train), 0);
+
+				fwrite(t, sizeof(Train), 1, stream);
+				t = t->next;
+
+			}
+
+			printf("Записано %d", list->count);
 			system("pause");
 			end = true;
 
